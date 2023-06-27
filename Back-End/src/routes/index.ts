49979c55718be206879e9dd2cfb5f078f5d168a8
Julian Navarro <julianrodrigonavarro@gmail.com>
2023-06-27 06:01:23 -0300

    Back Done || DB done

--- Back-End/src/routes/index.ts
+++ Back-End/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router, Request, Response } from "express";
 import pokemonRouter from "./moduleRoutes/pokemonRouter"
-import filtersRouter from "./moduleRoutes/filtersRouter"
 
 const router: Router = Router();
 router.use("/pokemons", pokemonRouter)
-router.use('/filters', filtersRouter)
 
 export default router
--- Back-End/src/routes/moduleRoutes/filtersRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import controllerDbPokemons from '../../controllers/controllerDbPokemons';
-
-const router: Router = Router();
-
-router.get('/dbpokemons', controllerDbPokemons)
-// router.get("")
-
-export default router;
